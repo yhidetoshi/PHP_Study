7f78b4eaebcf6fcaa1cb5903f09344b6f43f62fb
yajima <Hidetoshi_Yajima@voyagegroup.com>
2016-02-18 10:19:43 +0900

    Update basic2.php

--- basic2.php
+++ basic2.php
@@ -6,10 +6,6 @@ echo $buff - ($buff * (float)$m_p_q[2]/100);
 echo(PHP_EOL);
 
 /* 実行結果
-$in_m_p_q =fgets(STDIN);
-$m_p_q = explode(" ",$in_m_p_q);
-
-$buff = ((float)$m_p_q[0]) - ((float)$m_p_q[1]/100)*(float)$m_p_q[0];
-echo $buff - ($buff * (float)$m_p_q[2]/100);
-echo(PHP_EOL);
+15 20 30
+8.4
 */
