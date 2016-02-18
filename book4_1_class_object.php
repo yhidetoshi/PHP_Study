32f459360f60a6c146fd9cec766ee4a777cc5e0d
yajima <hyajima.jaist.job@gmail.com>
2016-02-19 08:36:20 +0900

    Create book4_1_class_object.php

---
+++ book4_1_class_object.php
@@ -0,0 +1,23 @@
+<?php
+
+class Employee
+{
+	public $name;
+	public $state = 'working_now';
+
+	// privateなのでクラスの内部からしかアクセスできない
+	private $state = 'working_now';
+
+	public function work()
+	{
+		echo '書類を整理しています' , PHP_EOL;
+	}
+}
+
+$yamada = new Employee();
+$yamada->name = 'yamada';
+echo $yamada->name, PHP_EOL;
+echo $yamada->state, PHP_EOL;
+$yamada->work();
+
+?>
